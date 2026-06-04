e96f584e345678e281be054b3e1a858b2c5347d2
farzanahNwt <farzanahnazri@gmail.com>
2026-06-04 22:53:57 +0800

    Initial Spring Boot Project

---
+++ src/main/java/dentist/dentalhub/DentalHubApplication.java
@@ -0,0 +1,13 @@
+package dentist.dentalhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dentist/dentalhub/DentalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dentist.dentalhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
